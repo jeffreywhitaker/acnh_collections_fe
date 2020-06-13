964b89ad18377b08670bcf8f7107b3110ccf5f58
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2020-06-13 00:27:51 -0700

    begin to set up landing page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 import NavBar from './components/NavBar'
+import LandingPage from './components/LandingPage'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
       <Route component={NavBar} />
+      <Route component={LandingPage} />
     </div>
   )
 }
---
+++ src/components/LandingPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import styled from 'styled-components'
+
+function LandingPage() {
+  return <div className="LandingPage"></div>
+}
+
+export default LandingPage
